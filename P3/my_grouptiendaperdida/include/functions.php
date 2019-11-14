f25573a761de5bfce4a9f37960bd0c570c4473bd
nachomestre <36193043+nachomestre@users.noreply.github.com>
2019-11-14 10:57:04 +0100

    Update functions.php

--- P3/my_grouptiendaperdida/include/functions.php
+++ P3/my_grouptiendaperdida/include/functions.php
@@ -50,11 +50,10 @@ function MP2_Register_Form($MP2_user , $user_email)
         <input type="text" name="email" class="item_requerid" size="20" maxlength="25" value="<?php print $MP2_user["email"] ?>"
         placeholder="kiko@ic.es" />
         <br/>
-	<label for="foto">Foto</label>
-	<br/>
-	<input type="file" name="fileToUpload" id="fileToUpload">
-	<br/>
-	<br/>
+		<label for="foto">Foto</label>
+		<br/>
+		<input type="file" name="foto" id="foto">
+		<br/>
         <input type="submit" value="Enviar">
         <input type="reset" value="Deshacer">
     </form>
@@ -100,13 +99,13 @@ function MP2_my_datos_2()
             //$pdo1 = new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_NAME, DB_USER, DB_PASSWORD); 
             $consult = $MP2_pdo->prepare($query);
             $a=$consult->execute($a);
-	    $fotoURL="";
-	    $IMAGENES_USUARIOS = '../fotos/';
-            if(array_key_exists('foto', $_FILES) && $_POST['email']) {
-	    $fotoURL = $IMAGENES_USUARIOS.$_POST['userName']."_".$_FILES['foto']['name'];
-	    if (move_uploaded_file($_FILES['fileToUpload']['tmp_name'], $fotoURL))
-	        { echo "foto subida con éxito";
-	    } }
+			$fotoURL="";
+			$IMAGENES_USUARIOS = '../fotos/';
+		    if(array_key_exists('foto', $_FILES) && $_REQUEST['email']) {
+				$fotoURL = $IMAGENES_USUARIOS.$_REQUEST['userName']."_".$_FILES['foto']['name'];
+				if (move_uploaded_file($_FILES['foto']['tmp_name'], $fotoURL))
+					{ echo "foto subida con éxito";
+		    } }
             if (1>$a) {echo "InCorrecto $query";}
             else wp_redirect(admin_url( 'admin-post.php?action=my_datos_2&proceso=listar'));
             break;
