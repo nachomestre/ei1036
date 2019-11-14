179e619dfdbceaa649e1464e82eee54e909303cc
nachomestre <36193043+nachomestre@users.noreply.github.com>
2019-11-14 12:05:48 +0100

    Update functions.php

--- P3/my_grouptiendaperdida/include/functions.php
+++ P3/my_grouptiendaperdida/include/functions.php
@@ -103,9 +103,10 @@ function MP2_my_datos_2()
 	    $fotoURL="";
 	    $IMAGENES_USUARIOS = '/../fotos/';
 	    if(array_key_exists('foto', $_FILES) && $_REQUEST['email']) {
-		echo var_dump($_FILES['foto']);
-		break;
 	    	$fotoURL = __DIR__.$IMAGENES_USUARIOS.$_REQUEST['userName']."_".$_FILES['foto']['name'];
+		echo var_dump($_FILES['foto']);
+		echo var_dump($fotoURL);
+		break;
 	    	if (move_uploaded_file($_FILES['foto']['tmp_name'], $fotoURL))
 	    		{ echo "foto subida con éxito";
 			wp_redirect(admin_url( 'admin-post.php?action=my_datos_2&proceso=registro'));
