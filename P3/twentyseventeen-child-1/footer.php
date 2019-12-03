d715823f0f324542f4c69c23096ab837ea104466
nachomestre <36193043+nachomestre@users.noreply.github.com>
2019-12-03 18:27:21 +0100

    Update footer.php

--- P3/twentyseventeen-child-1/footer.php
+++ P3/twentyseventeen-child-1/footer.php
@@ -15,7 +15,13 @@
 ?>
 
 		</div><!-- #content -->
-        <iframe id="iframe" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d6113.241528024085!2d-0.07195582232843417!3d39.994579537096755!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x1368bf53b3a7fb3f!2sUniversitat%20Jaume%20I!5e0!3m2!1ses!2ses!4v1575391088537!5m2!1ses!2ses" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
+		<!--Google maps-->
+        <div width="50%" margin="0 auto" class="wrap">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d6113.241528024085!2d-0.07195582232843417!3d39.994579537096755!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x1368bf53b3a7fb3f!2sUniversitat%20Jaume%20I!5e0!3m2!1ses!2ses!4v1575391088537!5m2!1ses!2ses" width="300" height="200" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
+            <div id="c_2e11f497ce68546345a8cf06534a2f14" class="mini"></div><script type="text/javascript" src="https://www.eltiempo.es/widget/widget_loader/2e11f497ce68546345a8cf06534a2f14"></script>
+        </div>
+        
+        
 		<footer id="colophon" class="site-footer" role="contentinfo">
 			<div class="wrap">
 				<?php
