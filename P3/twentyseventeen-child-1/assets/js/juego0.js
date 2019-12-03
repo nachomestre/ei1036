21be837c233bf7c377e788847c4ed357182d9f9f
nachomestre <36193043+nachomestre@users.noreply.github.com>
2019-12-03 19:14:01 +0100

    Update juego0.js

--- P3/twentyseventeen-child-1/assets/js/juego0.js
+++ P3/twentyseventeen-child-1/assets/js/juego0.js
@@ -1,49 +1,49 @@
-	<script type="text/javascript" charset="utf-8">
+<script type="text/javascript" charset="utf-8">
 
-		function getMousePos(canvas, evt) {
-				var rect = canvas.getBoundingClientRect();
-				return {
-					x: evt.clientX - rect.left,
-					y: evt.clientY - rect.top
-				};
-			}
-		function mostrarFoto(file, imagen) {
-			var reader = new FileReader();
-			reader.addEventListener("load", function () {
-				imagen.src = reader.result;
-			});
-			reader.readAsDataURL(file);
+	function getMousePos(canvas, evt) {
+			var rect = canvas.getBoundingClientRect();
+			return {
+				x: evt.clientX - rect.left,
+				y: evt.clientY - rect.top
+			};
 		}
-		
-		function limpiar(context) {
-			canvas = document.querySelector('#sketchpad');
-			context = canvas.getContext("2d");
-			context.clearRect(0, 0, canvas.width, canvas.height);
-		}
+	function mostrarFoto(file, imagen) {
+		var reader = new FileReader();
+		reader.addEventListener("load", function () {
+			imagen.src = reader.result;
+		});
+		reader.readAsDataURL(file);
+	}
 
-		function dibuja(context) {
-			context.fillStyle = "rgb(200,0,0)";
-			context.fillRect(20, 10, 40, 50);
-		}
-		function DibujaEnRaton(context, coors) {
-			context.fillStyle = "rgb(200,200,0)";
-			context.fillRect(coors.x, coors.y, 30, 30);
+	function limpiar(context) {
+		canvas = document.querySelector('#sketchpad');
+		context = canvas.getContext("2d");
+		context.clearRect(0, 0, canvas.width, canvas.height);
+	}
 
-				
-			}
-		function ready() {
-			var canvas = document.querySelector("#sketchpad");
-			context = canvas.getContext('2d');
-			canvas.addEventListener("click",function(evt){
-				coors=getMousePos(canvas, evt);
-				DibujaEnRaton(context, coors) ;
-			})
-			document.querySelector("#dibujar").addEventListener("click", function () {
-				dibuja(context);
-			});
-			document.querySelector("#limpiar").addEventListener("click", function () {
-				limpiar(context);
-			});
+	function dibuja(context) {
+		context.fillStyle = "rgb(200,0,0)";
+		context.fillRect(20, 10, 40, 50);
+	}
+	function DibujaEnRaton(context, coors) {
+		context.fillStyle = "rgb(200,200,0)";
+		context.fillRect(coors.x, coors.y, 30, 30);
+
+
 		}
-		ready();
-	</script>
+	function ready() {
+		var canvas = document.querySelector("#sketchpad");
+		context = canvas.getContext('2d');
+		canvas.addEventListener("click",function(evt){
+			coors=getMousePos(canvas, evt);
+			DibujaEnRaton(context, coors) ;
+		})
+		document.querySelector("#dibujar").addEventListener("click", function () {
+			dibuja(context);
+		});
+		document.querySelector("#limpiar").addEventListener("click", function () {
+			limpiar(context);
+		});
+	}
+	ready();
+</script>
