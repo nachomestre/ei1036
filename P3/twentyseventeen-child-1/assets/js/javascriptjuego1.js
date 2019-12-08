4feeea7bfe660649bba021d828f5f358136c9f12
nachomestre <36193043+nachomestre@users.noreply.github.com>
2019-12-08 11:10:34 +0100

    Create javascriptjuego1.js

---
+++ P3/twentyseventeen-child-1/assets/js/javascriptjuego1.js
@@ -0,0 +1,49 @@
+var cuadrado = [0, 0];
+function getMousePos(canvas, evt) {
+		var rect = canvas.getBoundingClientRect();
+		return {
+			x: evt.clientX - rect.left,
+			y: evt.clientY - rect.top
+		};
+	}
+
+function limpiar(context) {
+	canvas = document.querySelector('#sketchpad');
+	context = canvas.getContext("2d");
+	context.clearRect(0, 0, canvas.width, canvas.height);
+}
+
+function dibuja(context) {
+	var x = Math.floor(Math.random() * 470);
+	var y = Math.floor(Math.random() * 470);
+	cuadrado[0] = x;
+	cuadrado[1] = y;
+	context.fillStyle = "rgb(0,0,200)";
+	context.fillRect(x, y, 30, 30);
+}
+function DibujaEnRaton(context, coors) {
+	if(coors.x > cuadrado[0] && coors.x < cuadrado[0]+30 && coors.y > cuadrado[1] && coors.y < cuadrado[1]+30){
+		context.fillStyle = "rgb(200,0,0)";
+		context.fillRect(cuadrado[0], cuadrado[1], 30, 30);
+		dibuja(context);
+	}
+		
+	}
+function ready() {
+	var canvas = document.querySelector("#sketchpad");
+	context = canvas.getContext('2d');
+	canvas.addEventListener("click",function(evt){
+		coors=getMousePos(canvas, evt);
+		DibujaEnRaton(context, coors, cuadrado) ;
+	})
+	document.querySelector("#reiniciar").addEventListener("click", function () {
+		limpiar(context);
+		dibuja(context, cuadrado);
+	});
+	
+	dibuja(context);
+}
+
+window.addEventListener('DOMContentLoaded', (event) => {
+    ready();
+});
