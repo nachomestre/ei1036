7cc7840a4c573435019a567daa2831302b8fb269
nachomestre <36193043+nachomestre@users.noreply.github.com>
2019-12-08 10:10:29 +0100

    Add files via upload

---
+++ P3/twentyseventeen-child-1/assets/js/javascriptjuego0.js
@@ -0,0 +1,42 @@
+function getMousePos(canvas, evt) {
+		var rect = canvas.getBoundingClientRect();
+		return {
+			x: evt.clientX - rect.left,
+			y: evt.clientY - rect.top
+		};
+	}
+
+function limpiar(context) {
+	canvas = document.querySelector('#sketchpad');
+	context = canvas.getContext("2d");
+	context.clearRect(0, 0, canvas.width, canvas.height);
+}
+
+function dibuja(context) {
+	context.fillStyle = "rgb(200,0,0)";
+	context.fillRect(20, 10, 40, 50);
+}
+function DibujaEnRaton(context, coors) {
+	context.fillStyle = "rgb(200,200,0)";
+	context.fillRect(coors.x, coors.y, 30, 30);
+
+		
+	}
+function ready() {
+	var canvas = document.querySelector("#sketchpad");
+	context = canvas.getContext('2d');
+	canvas.addEventListener("click",function(evt){
+		coors=getMousePos(canvas, evt);
+		DibujaEnRaton(context, coors) ;
+	})
+	document.querySelector("#dibujar").addEventListener("click", function () {
+		dibuja(context);
+	});
+	document.querySelector("#limpiar").addEventListener("click", function () {
+		limpiar(context);
+	});
+}
+
+window.addEventListener('DOMContentLoaded', (event) => {
+    ready();
+});
