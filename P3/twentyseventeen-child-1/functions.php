8c7f3a5bb363b235f5c77d024645231527ce70ad
nachomestre <36193043+nachomestre@users.noreply.github.com>
2019-12-08 11:17:50 +0100

    Update functions.php

--- P3/twentyseventeen-child-1/functions.php
+++ P3/twentyseventeen-child-1/functions.php
@@ -45,7 +45,7 @@
 	}
 	function add_theme_scripts1(){
 		wp_enqueue_script( 'miscript', get_stylesheet_directory_uri().'/assets/js/javascriptjuego1.js',true);
-		//wp_enqueue_style( 'style',get_stylesheet_directory_uri().'/assets/css/micss.js',true); // juego nuevo
+		//wp_enqueue_style( 'style',get_stylesheet_directory_uri().'/assets/css/micss.js',true);
 	}
 	add_shortcode('juego1', 'shortcode_juego1');
 	add_action( 'wp_enqueue_scripts', 'add_theme_scripts1' );
