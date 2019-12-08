4b43668ad57e17e1c101c485412520ef6f0cabf5
nachomestre <36193043+nachomestre@users.noreply.github.com>
2019-12-08 11:09:40 +0100

    Update functions.php

--- P3/twentyseventeen-child-1/functions.php
+++ P3/twentyseventeen-child-1/functions.php
@@ -34,6 +34,24 @@
 	add_shortcode('juego0', 'shortcode_juego0');
 	add_action( 'wp_enqueue_scripts', 'add_theme_scripts' );
 
+	function shortcode_juego1() {
+	  return '
+	  	<div class="wrap">
+		<canvas id="sketchpad1" width="500" height="500" style="background-color: coral;"></canvas>
+		<p> <span id="reiniciar1"> REINICIAR </span>
+		</p>
+		</div>
+	';
+	}
+	function add_theme_scripts1(){
+		wp_enqueue_script( 'miscript', get_stylesheet_directory_uri().'/assets/js/javascriptjuego1.js',true);
+		//wp_enqueue_style( 'style',get_stylesheet_directory_uri().'/assets/css/micss.js',true);
+	}
+	add_shortcode('juego1', 'shortcode_juego1');
+	add_action( 'wp_enqueue_scripts', 'add_theme_scripts1' );
+
+
+
 	// Registramos el widget
 	function load_my_widget() {
 		register_widget( 'my_widget1' );
