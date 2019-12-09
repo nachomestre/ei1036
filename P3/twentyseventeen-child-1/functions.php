0e9ad71fbd388fa0d7e3501360e70fadec8025e9
nachomestre <36193043+nachomestre@users.noreply.github.com>
2019-12-09 08:28:28 +0100

    Update functions.php

--- P3/twentyseventeen-child-1/functions.php
+++ P3/twentyseventeen-child-1/functions.php
@@ -27,12 +27,12 @@
 		</div>
 	';
 	}
-	function add_theme_scripts(){
-		wp_enqueue_script( 'miscript', get_stylesheet_directory_uri().'/assets/js/javascriptjuego0.js',true);
+	function add_theme_scripts0(){
+		wp_enqueue_script( 'juego0', get_stylesheet_directory_uri().'/assets/js/javascriptjuego0.js',true);
 		//wp_enqueue_style( 'style',get_stylesheet_directory_uri().'/assets/css/micss.js',true); //todavia no hay
 	}
 	add_shortcode('juego0', 'shortcode_juego0');
-	add_action( 'wp_enqueue_scripts', 'add_theme_scripts' );
+	add_action( 'wp_enqueue_scripts', 'add_theme_scripts0' );
 
 	function shortcode_juego1() {
 	  return '
@@ -44,7 +44,7 @@
 	';
 	}
 	function add_theme_scripts1(){
-		wp_enqueue_script( 'miscript', get_stylesheet_directory_uri().'/assets/js/javascriptjuego1.js',true);
+		wp_enqueue_script( 'juego1', get_stylesheet_directory_uri().'/assets/js/javascriptjuego1.js',true);
 		//wp_enqueue_style( 'style',get_stylesheet_directory_uri().'/assets/css/micss.js',true);
 	}
 	add_shortcode('juego1', 'shortcode_juego1');
