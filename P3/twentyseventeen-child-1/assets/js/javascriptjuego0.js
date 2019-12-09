beaaffe4b8d441ccc5feefef7a2047a06321d9a3
nachomestre <36193043+nachomestre@users.noreply.github.com>
2019-12-09 08:51:40 +0100

    Update javascriptjuego0.js

--- P3/twentyseventeen-child-1/assets/js/javascriptjuego0.js
+++ P3/twentyseventeen-child-1/assets/js/javascriptjuego0.js
@@ -40,14 +40,16 @@ function dibujaPremio(context){
 function DibujaEnRaton(context, coors) {
 	var vector = [coors.x - cuadrado[0], coors.y - cuadrado[1]];
 	var modulo = Math.sqrt(vector[0]*vector[0]+vector[1]*vector[1]);
-	context.fillStyle = "rgb(0,0,200)";
-	context.fillRect(cuadrado[0], cuadrado[1], 10, 10);
-	cuadrado[0] = cuadrado[0] + vector[0] / modulo * 10;
-	cuadrado[1] = cuadrado[1] + vector[1] / modulo * 10;
-	if(context.getImageData(cuadrado[0], cuadrado[1], 1, 1).data[2]==200){
+	vector[0] = cuadrado[0] + vector[0] / modulo * 10;
+	vector[1] = cuadrado[1] + vector[1] / modulo * 10;
+	if(context.getImageData(vector[0], vector[1], 1, 1).data[2]==200){
 		premio[0] = -200;
 		premio[1] = -200;
 	}
+	context.fillStyle = "rgb(0,0,200)";
+	context.fillRect(cuadrado[0], cuadrado[1], 10, 10);
+	cuadrado[0] = vector[0];
+	cuadrado[1] = vector[1];
 	context.fillStyle = "rgb(200,0,0)";
 	context.fillRect(cuadrado[0], cuadrado[1], 10, 10);
 	if(cuadrado[0]+5 > premio[0] && cuadrado[0]+5 < premio[0]+30 && cuadrado[1]+5 > premio[1] && cuadrado[1]+5 < premio[1]+30){
