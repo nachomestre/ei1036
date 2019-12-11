f5be084523b7e75b1ee1c2ec57ff005d633cd7f2
nachomestre <36193043+nachomestre@users.noreply.github.com>
2019-12-11 09:44:54 +0100

    Update javascriptjuego1.js

--- P3/twentyseventeen-child-1/assets/js/javascriptjuego1.js
+++ P3/twentyseventeen-child-1/assets/js/javascriptjuego1.js
@@ -1,5 +1,5 @@
-var cuadrado = [0, 0];
-function getMousePos(canvas, evt) {
+var cuadrado1 = [0, 0];
+function getMousePos1(canvas, evt) {
 		var rect = canvas.getBoundingClientRect();
 		return {
 			x: evt.clientX - rect.left,
@@ -7,46 +7,46 @@ function getMousePos(canvas, evt) {
 		};
 	}
 
-function limpiar(context) {
+function limpiar1(context) {
 	canvas = document.querySelector('#sketchpad1');
 	context = canvas.getContext("2d");
 	context.clearRect(0, 0, canvas.width, canvas.height);
 }
 
-function dibuja(context) {
+function dibuja1(context) {
 	var x = Math.floor(Math.random() * 470);
 	var y = Math.floor(Math.random() * 470);
-	cuadrado[0] = x;
-	cuadrado[1] = y;
+	cuadrado1[0] = x;
+	cuadrado1[1] = y;
 	context.fillStyle = "rgb(0,0,200)";
 	context.fillRect(x, y, 30, 30);
 }
-function DibujaEnRaton(context, coors) {
-	if(coors.x > cuadrado[0] && coors.x < cuadrado[0]+30 && coors.y > cuadrado[1] && coors.y < cuadrado[1]+30){
+function DibujaEnRaton1(context, coors) {
+	if(coors.x > cuadrado1[0] && coors.x < cuadrado1[0]+30 && coors.y > cuadrado1[1] && coors.y < cuadrado1[1]+30){
 		context.fillStyle = "rgb(200,0,0)";
 		context.fillRect(cuadrado[0], cuadrado[1], 30, 30);
 		dibuja(context);
 	}
 		
 	}
-function ready() {
+function ready1() {
 	var canvas = document.querySelector("#sketchpad1");
 	context = canvas.getContext('2d');
 	canvas.addEventListener("click",function(evt){
-		coors=getMousePos(canvas, evt);
-		DibujaEnRaton(context, coors, cuadrado) ;
+		coors=getMousePos1(canvas, evt);
+		DibujaEnRaton1(context, coors, cuadrado) ;
 	})
 	document.querySelector("#reiniciar1").addEventListener("click", function () {
-		limpiar(context);
-		dibuja(context, cuadrado);
+		limpiar1(context);
+		dibuja1(context, cuadrado);
 	});
 	
-	dibuja(context);
+	dibuja1(context);
 }
 
 window.addEventListener('DOMContentLoaded', (event) => {
-    var selection = document.querySelector('#sketchpad1') != null;
-    if (selection) {
-        //ready();
+    var selection1 = document.querySelector('#sketchpad1') != null;
+    if (selection1) {
+        ready1();
     }
 });
