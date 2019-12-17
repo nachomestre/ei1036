6be7ea9d2e223a4fe9767133dc4a68e11f57e3c5
nachomestre <36193043+nachomestre@users.noreply.github.com>
2019-12-17 16:52:59 +0100

    Update my_grouptiendaperdida.php

--- P3/my_grouptiendaperdida/my_grouptiendaperdida.php
+++ P3/my_grouptiendaperdida/my_grouptiendaperdida.php
@@ -33,7 +33,7 @@ register_activation_hook( __FILE__, 'MP2_Ejecutar_crearT');
 function MP2_Ejecutar_crearT(){
     MP2_CrearT("MP2_GrupoCliente001");
 }
-//add_action('admin_post_nopriv_my_datos_2', 'MP2_my_datos_2'); //no autentificados
-add_action('admin_post_my_datos_2', "MP2_my_datos_2"); 
+add_action('admin_post_nopriv_my_datos_2', 'MP2_my_datos_2'); //no autentificados
+//add_action('admin_post_my_datos_2', "MP2_my_datos_2"); 
 
 ?>
