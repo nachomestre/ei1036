359b67ab8fa55689d3358e9df3287e7b555103cd
nachomestre <36193043+nachomestre@users.noreply.github.com>
2019-12-17 16:30:58 +0100

    Update my_group1.php

--- P3/my_group1/my_group1.php
+++ P3/my_group1/my_group1.php
@@ -2,9 +2,8 @@
 /*
 Plugin Name: my_group1
 Description: Register group of persons.
-Author URI: lola L
-Author Email: dllido@uji.es
-Version: 1.0
+Author URI: Nacho M
+Version: 2.0
 License: GPLv3
 License URI: http://www.gnu.org/licenses/gpl-3.0.html
 */
