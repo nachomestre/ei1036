80cceb05e9fb1f5fed57ba83ef046414728087b0
nachomestre <36193043+nachomestre@users.noreply.github.com>
2019-12-20 15:42:55 +0100

    Update my_grouptiendaperdida.php

--- P3/my_grouptiendaperdida/my_grouptiendaperdida.php
+++ P3/my_grouptiendaperdida/my_grouptiendaperdida.php
@@ -24,7 +24,7 @@ License URI: http://www.gnu.org/licenses/gpl-3.0.html
 
 //La siguiente sentencia activaria la acción para todos los usuarios.
 //add_action('admin_post_nopriv_my_datos_2', 'my_datos_2');
-$table="MP2_GrupoCliente003";
+$table="MP2_GrupoCliente002";
 
 include(plugin_dir_path( __FILE__ ).'include/functions.php');
 
@@ -32,9 +32,9 @@ register_activation_hook( __FILE__, 'MP2_Ejecutar_crearT');
 
 //add_action( 'plugins_loaded', 'Ejecutar_crearT' ); // esto se ejecuta siempre que se llama al plugin
 function MP2_Ejecutar_crearT(){
-    MP2_CrearT("MP2_GrupoCliente003");
+    MP2_CrearT("MP2_GrupoCliente002");
 }
-add_action('admin_post_nopriv_my_datos_3', 'MP2_my_datos_3'); //no autentificados
-//add_action('admin_post_my_datos_3', "MP2_my_datos_3"); 
+add_action('admin_post_nopriv_my_datos_2', 'MP2_my_datos_2'); //no autentificados
+//add_action('admin_post_my_datos_2', "MP2_my_datos_2"); 
 
 ?>
